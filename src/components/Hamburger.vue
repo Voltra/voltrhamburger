f322d9eb77e4023ec1a9609c7e0db4a4024d8edd
Ludwig GUERIN <ludwig.guerin@swpl.fr>
2020-07-30 10:54:17 +0200

    Initial setup and hamburger setup

---
+++ src/components/Hamburger.vue
@@ -0,0 +1,39 @@
+<template>
+    <button @click="toggle($event)" :class="classes">
+        <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+        </span>
+    </button>
+</template>
+
+<script>
+    import VueTypes from "vue-types"
+    export default {
+        /* data(){
+            return {
+                opened: false,
+            };
+        }, */
+        props: {
+            opened: VueTypes.bool.def(false),
+        },
+        computed: {
+            classes(){
+                const cls = "hamburger hamburger--emphatic";
+                return this.opened ? `${cls} is-active` : cls;
+            }
+        },
+        methods: {
+            toggle(e){
+                /* this.opened = !this.opened;
+                const eventName = this.opened ? "open" : "close"; */
+                const eventName = this.opened ? "close" : "open";
+                this.$emit(eventName, e);
+            }
+        }
+    }
+</script>
+
+
+<style lang="scss" scoped>
+</style>
